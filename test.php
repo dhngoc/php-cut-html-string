3bb0cd248e60c964d4280b9ccb96612e086795d9
Dinh Hong Ngoc <todo@imind.systems>
2014-09-21 08:59:27 -0700

    Revert "CUT HTML STRING"
    This reverts commit dd2122e8709f4e5e2813b9ea6ee1e1558ce64423.

--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-require "function.php";
-$html_str = '<p>Even though he is suspended, Winston entered the field in full pads and uniform and proceeded to warm up with the <a href="/collegefootball/team/florida-state-seminoles-football/86043" data-cqtag="NCAA/CFB/ACC/Florida_State">Seminoles</a>.</p>';
-echo cutHtmlString($html_str);
