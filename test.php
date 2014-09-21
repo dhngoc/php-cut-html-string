d4918c0711669c8ed072f0f070f58b0a62d48e45
Dinh Hong Ngoc <todo@imind.systems>
2014-09-21 09:24:13 -0700

    Comment description

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
 require "function.php";
+// String contains HTML tags
 $html_str = '<p>Even though he is suspended, Winston entered the field in full pads and uniform and proceeded to warm up with the <a href="/collegefootball/team/florida-state-seminoles-football/86043" data-cqtag="NCAA/CFB/ACC/Florida_State">Seminoles</a>.</p>';
 echo cutHtmlString($html_str);
